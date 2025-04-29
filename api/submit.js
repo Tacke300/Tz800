9a6d08b57d13278169783784816369e130592352
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-04-29 09:50:28 +0700

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tramnanrzruzvkehpydl.supabase.co '; // <-- Thay URL Supabase của bạn
+const supabaseUrl = 'https://tramnanrzruzvkehpydl.supabase.co'; // <-- Thay URL Supabase của bạn
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYW1uYW5yenJ1enZrZWhweWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NTM1NTMsImV4cCI6MjA2MTMyOTU1M30.L0Ytkxi80AbYjkjpDfGyQtfyfqjfHLF98OrVce9Hi-0'; // <-- Thay Anon Key của bạn
 const supabase = createClient(supabaseUrl, supabaseKey);
 
