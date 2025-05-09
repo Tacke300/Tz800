2fc8e38fb3e7bd4c35697202a5cddae8cd9b3bed
tacke300 <tacke300@gmail.com>
2025-05-09 05:36:44 +0000

    add auto_pull.sh

---
+++ auto_pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+WORK_DIR=~
+cd "$WORK_DIR" || { echo "Không thể di chuyển đến thư mục $WORK_DIR"; exit 1; }
+if [ -d ".git" ]; then
+    echo "Đang thực hiện git pull..."
+    git pull origin main || { echo "Lỗi khi pull từ git!"; exit 1; }
+else
+    echo "Không phải là git repository. Bỏ qua git pull."
+fi
+echo "Hoàn thành pull và các tác vụ khác!"
