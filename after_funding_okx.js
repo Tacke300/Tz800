b3f2b33f45731bd014ee1ac938f006b353b6d653
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-05-19 13:19:13 +0700

    Update after_funding_okx.js

--- after_funding_okx.js
+++ after_funding_okx.js
@@ -13,6 +13,9 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  res.send('OK');
+});
 
 
 // Supabase config
