8a43a893629bccd31d07b91cb7e971074aa65502
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-05-19 19:28:47 +0700

    Update after_funding_okx.js

--- after_funding_okx.js
+++ after_funding_okx.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Supabase config
 const supabaseUrl = 'https://tramnanrzruzvkehpydl.supabase.co';
-const supabaseKey = 'motconvithaiconvit';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYW1uYW5yenJ1enZrZWhweWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NTM1NTMsImV4cCI6MjA2MTMyOTU1M30.L0Ytkxi80AbYjkjpDfGyQtfyfqjfHLF98OrVce9Hi-0';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 let botRunning = false;
