d660d8dba56f007315b583d58e1460c74301dcc4
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-05-19 13:11:59 +0700

    Update after_funding_okx.js

--- after_funding_okx.js
+++ after_funding_okx.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
