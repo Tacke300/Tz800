abed9ab0d0ad1e38009fc737ab1dcc0dc7b1ecf9
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-05-24 16:54:09 +0700

    Update reg-log.js

--- all-js/reg-log.js
+++ all-js/reg-log.js
@@ -1,4 +1,4 @@
-const supabase = supabase.createClient(
+const supabase = window.supabase.createClient(
   'https://tramnanrzruzvkehpydl.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYW1uYW5yenJ1enZrZWhweWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NTM1NTMsImV4cCI6MjA2MTMyOTU1M30.L0Ytkxi80AbYjkjpDfGyQtfyfqjfHLF98OrVce9Hi-0'
 );
